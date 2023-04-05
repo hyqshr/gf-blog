eadd563870ead97cb5497e08b3b18e097bdd7948
Yiqiu <hyiqiu23@gmail.com>
2023-04-04 23:02:15 -0400

    Updates

---
+++ precache-manifest.90c9afbedc3cbfa2f0296005ef2e05c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7951238911b9f9bf6ce824c00e7f7fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515cd8cb52525f955961",
+    "url": "/static/css/2.8dc29398.chunk.css"
+  },
+  {
+    "revision": "52b7f33e9b357604ec12",
+    "url": "/static/css/main.8c7179da.chunk.css"
+  },
+  {
+    "revision": "515cd8cb52525f955961",
+    "url": "/static/js/2.92d10af4.chunk.js"
+  },
+  {
+    "revision": "52b7f33e9b357604ec12",
+    "url": "/static/js/main.452b2cb5.chunk.js"
+  },
+  {
+    "revision": "0a0e518b72429dcf8752",
+    "url": "/static/js/runtime-main.70904e36.js"
+  },
+  {
+    "revision": "8a5cac0c78394d2fc42b0b10f6350b30",
+    "url": "/static/media/gf2.8a5cac0c.jpeg"
+  }
+]);
